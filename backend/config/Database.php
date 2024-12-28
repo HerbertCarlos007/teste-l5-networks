834643c672a044bcc9ff1e4c714cf1b830d7fc66
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-12-28 01:47:27 -0300

    feat: inicializando a conexao com o banco de dados

---
+++ backend/config/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+class Database
+{
+    protected static $db;
+
+    private function __construct()
+    {
+        $driver = "mysql";
+        $host = "localhost";
+        $dbname = "starwars";
+        $username = "root";
+        $password = "";
+
+        try {
+           self::$db = new PDO("$driver:host=$host;dbname=$dbname", $username, $password);
+    
+           self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+           self::$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+           echo "Conexão com o banco de dados realizada com sucesso!";
+        } catch (PDOException $e) {
+        
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public static function getConection()
+    {
+        if (!self::$db) {
+            new Database();
+        }
+
+        return self::$db;
+    }
+}
+
+?>
---
+++ backend/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once("config/Database.php");
+
+$con = Database::getConection()
+?>
